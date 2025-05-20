3212ef3fbe9a0ab79fd01363a3eeddbf08b09d33
mattgasiorek <matthew.gasiorek.coding@gmail.com>
2025-05-19 22:01:48 -0400

    innitial commit of my project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+//Project Name: Temperature Converter
+//Author: Matthew Gasiorek
+//Created On: 2025-05-19
+//Last Modified: 2025-05-19
+//Description: I want this to be a fun, introductory project where I can learn the basics of Rust. Rust seems like a cool language that is C-adjacent.
+//Version: 1.0
+//Contact: matthew.gasiorek.coding@gmail.com
+
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+}
